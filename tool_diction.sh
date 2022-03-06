75baee071706ec1ce1298a5ba1d4a0e226c0bd36
bencordeiro <94664097+bencordeiro@users.noreply.github.com>
2022-03-06 15:02:51 -0500

    Update tool_diction.sh

--- tool_diction.sh
+++ tool_diction.sh
@@ -7,6 +7,7 @@ tool_diction()
 	echo "Volatility - Memory forensics | Mostly used for .img files"
 	echo "Exiftool - metadata recorder built on perl | With a wide range of file types."
 	echo "CeWL - Powerful custom work list generator."
+	echo "pngcheck - Print info and check png"
 	
     ### Dep Check
 }
